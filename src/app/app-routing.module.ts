54b13ef72394f3911f325f9cbebf0eaa4770260e
colbyallenc <colbyallenc@gmail.com>
2018-10-30 09:55:38 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { OffersComponent } from './offers/offers.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'accomodation', component: AccomodationComponent},
+  {path: 'celebrations', component: CelebrationsComponent},
+  {path: 'food-beverage', component: FoodBeverageComponent},
+  {path: 'lifestyle', component: LifestyleComponent},
+  {path: 'weddings', component: WeddingsComponent},
+  {path: 'offers', component: OffersComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatBadgeModule} from '@angular/material'
+import {MatButtonModule, MatCheckboxModule, MatBadgeModule} from '@angular/material';
+
 
 @NgModule({
     imports: [MatBadgeModule, MatButtonModule, MatCheckboxModule],
