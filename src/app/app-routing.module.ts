a2b6b9cb5f2d305bd8d21cb778bd852b20b15ee8
colbyallenc <colbyallenc@gmail.com>
2018-10-30 11:12:50 -0400

    navbar routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
 import { LifestyleComponent } from './lifestyle/lifestyle.component';
 import { WeddingsComponent } from './weddings/weddings.component';
 import { OffersComponent } from './offers/offers.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
@@ -15,11 +18,14 @@ const routes: Routes = [
   {path: 'lifestyle', component: LifestyleComponent},
   {path: 'weddings', component: WeddingsComponent},
   {path: 'offers', component: OffersComponent},
-  
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
